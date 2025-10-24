4b1df7ee10d064d3efef21f7a675c3331da02e46
Ramon Prieto <rprieto@etched.com>
2025-10-24 16:06:24 -0700

    update

--- tensorrt_llm/llmapi/llm.py
+++ tensorrt_llm/llmapi/llm.py
@@ -287,6 +287,7 @@ class BaseLLM:
         Returns:
             Union[tensorrt_llm.llmapi.RequestOutput, List[tensorrt_llm.llmapi.RequestOutput]]: The output data of the completion request to the LLM.
         """
+        print(f"XXXXXX: Generate")
         unbatched = not isinstance(inputs, list)
         if not unbatched:
             if isinstance(inputs[0], int):
