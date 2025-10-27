f793c30f1a961bff546d4fbfb8275694ede92d5a
Ramon Prieto <rprieto@etched.com>
2025-10-27 09:31:03 -0700

    update

--- tensorrt_llm/llmapi/ramon.py
+++ tensorrt_llm/llmapi/ramon.py
@@ -1,7 +1,7 @@
 import inspect
 import os
 
-def get_caller_info():
+def log(msg: str):
     caller_frame = inspect.stack()[1]
     info = inspect.getframeinfo(caller_frame.frame)
 
@@ -16,9 +16,6 @@ def get_caller_info():
     elif 'cls' in caller_frame.frame.f_locals:
         class_name = caller_frame.frame.f_locals['cls'].__name__
 
+    print(f"XXXX::{filename}::{class_name}::{function_name}::{line_number}::{msg}")
     return filename, class_name, function_name, line_number
-
-def log(msg: str):
-    fn, cn, fn, ln = get_caller_info()
-    print(f"XXXX::{fn}::{cn}::{fn}::{ln}::{msg}")
 
