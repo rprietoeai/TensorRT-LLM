5619ffa9ec7475241e7fa7188e9326bc7cfe3fd2
Ramon Prieto <rprieto@etched.com>
2025-10-27 10:47:14 -0700

    update

--- tensorrt_llm/executor/worker.py
+++ tensorrt_llm/executor/worker.py
@@ -408,6 +408,7 @@ def worker_main(
                         "Failed to deliver ready signal to proxy, continuing anyway"
                     )
                 while (req := request_queue.get()) is not None:
+                    ramon.log("Inside while request_queue.get")
                     if isinstance(req, CancellingRequest):
                         worker.abort_request(req.id)
                     elif isinstance(req, GenerationRequest):
