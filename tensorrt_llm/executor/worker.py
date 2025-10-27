6e9c7367aec28b457649f9f214878c4ef5bb3e11
Ramon Prieto <rprieto@etched.com>
2025-10-27 10:54:16 -0700

    update

--- tensorrt_llm/executor/worker.py
+++ tensorrt_llm/executor/worker.py
@@ -415,6 +415,7 @@ def worker_main(
                     elif isinstance(req, GenerationRequest):
                         ramon.log("It's a generation request")
                         try:
+                            ramon.log(f"worker class: {str(type(worker))}")
                             ramon.log("Submitting request to worker")
                             worker.submit(req)
                         except RequestError as e:
