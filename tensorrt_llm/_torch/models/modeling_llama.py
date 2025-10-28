49b78a9681cc33c3f46f4ba8ccf53393a1d91836
Ramon Prieto <rprieto@etched.com>
2025-10-28 14:49:43 -0700

    update

--- tensorrt_llm/_torch/models/modeling_llama.py
+++ tensorrt_llm/_torch/models/modeling_llama.py
@@ -962,6 +962,7 @@ class LlamaModel(DecoderModel):
 
         for decoder_layer in self.layers[:self.num_hidden_layers]:
             ramon.log("Inside decoder layer")
+            ramon.log(f"decoder layer type: {str(type(decoder_layer))}")
             hidden_states, residual = decoder_layer(
                 position_ids=position_ids,
                 hidden_states=hidden_states,
