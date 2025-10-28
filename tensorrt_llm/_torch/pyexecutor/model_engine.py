259f2e0aa0dd455d5105f2e6f0c6b1a3a0645d01
Ramon Prieto <rprieto@etched.com>
2025-10-28 13:58:05 -0700

    update

--- tensorrt_llm/_torch/pyexecutor/model_engine.py
+++ tensorrt_llm/_torch/pyexecutor/model_engine.py
@@ -2362,6 +2362,7 @@ class PyTorchModelEngine(ModelEngine):
             return trace_func(self.model.forward)(**kwargs)
         else:
             ramon.log("Inside Inside the non torch compile, non tllm")
+            ramon.log(f"model class: {str(dtype(self.model))}")
             return self.model.forward(**kwargs)
 
     @nvtx_range("_forward_step")
