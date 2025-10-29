052ef53ecd4f2f16923ab632d3f7b9b2c080a349
Ramon Prieto <rprieto@etched.com>
2025-10-29 12:40:44 -0700

    update

--- tensorrt_llm/_torch/modules/linear.py
+++ tensorrt_llm/_torch/modules/linear.py
@@ -795,6 +795,9 @@ class NVFP4LinearMethod(LinearMethodBase):
             ramon.log("---|---|--->Right before fp4_quantize")
             act_fp4, act_sf = torch.ops.trtllm.fp4_quantize(
                 input, module.input_scale, module.scaling_vector_size, False)
+            ramon.log(f"---|---|--->act_fp4 type: {type(act_fp4)}")
+            ramon.log(f"---|---|--->act_sf type: {type(act_fp4)}")
+
 
         if IS_CUTLASS_DSL_AVAILABLE and module.use_cute_dsl_nvfp4_blockscaling_mm:
             ramon.log("---|---|--->cutlass is available and we can use cute nvfp4 mm")
