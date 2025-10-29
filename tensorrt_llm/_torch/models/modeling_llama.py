9531cdd231d1f5a3431ae6569801899174072292
Ramon Prieto <rprieto@etched.com>
2025-10-29 11:27:23 -0700

    update

--- tensorrt_llm/_torch/models/modeling_llama.py
+++ tensorrt_llm/_torch/models/modeling_llama.py
@@ -703,11 +703,11 @@ class LlamaDecoderLayer(DecoderLayer):
 
         # Self Attention
         ramon.log("XXXXXXXXXXXXXXXXx")
-        ramon.log(f"    is_nvfp4: {str(self.is_nvfp4)}")
-        ramon.log(f"    attention class: {str(type(self.self_attn))}")
-        ramon.log(f"    position_ids: {str(position_ids.dtype)}")
-        ramon.log(f"    hidden_states: {str(hidden_states.dtype)}")
-        ramon.log(f"    hidden_states shape: {str(hidden_states.shape)}")
+        ramon.log(f"is_nvfp4: {str(self.is_nvfp4)}")
+        ramon.log(f"attention class: {str(type(self.self_attn))}")
+        ramon.log(f"position_ids: {str(position_ids.dtype)}")
+        ramon.log(f"hidden_states: {str(hidden_states.dtype)}")
+        ramon.log(f"hidden_states shape: {str(hidden_states.shape)}")
         hidden_states = self.self_attn(
             position_ids=position_ids,
             hidden_states=hidden_states,
