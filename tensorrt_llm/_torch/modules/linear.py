301f37f34eb86915b7622a091f72bf01346c72c1
Ramon Prieto <rprieto@etched.com>
2025-10-29 12:55:51 -0700

    update

--- tensorrt_llm/_torch/modules/linear.py
+++ tensorrt_llm/_torch/modules/linear.py
@@ -796,6 +796,7 @@ class NVFP4LinearMethod(LinearMethodBase):
             act_fp4, act_sf = torch.ops.trtllm.fp4_quantize(
                 input, module.input_scale, module.scaling_vector_size, False)
             ramon.log(f"---|---|--->act_fp4 dtype: {act_fp4.dtype}")
+            ramon.log(f"---|---|--->act_fp4 shape: {act_fp4.shape}")
             ramon.log(f"---|---|--->act_sf dtype: {act_sf.dtype}")
 
 
