02886171f25d72f1d9fa2a96eca144994ea589a5
Ramon Prieto <rprieto@etched.com>
2025-10-29 14:07:05 -0700

    update

--- tensorrt_llm/_torch/modules/linear.py
+++ tensorrt_llm/_torch/modules/linear.py
@@ -819,7 +819,7 @@ class NVFP4LinearMethod(LinearMethodBase):
             ramon.log(f"---|---|--->modul weight shape: {module.weight.shape}")
             ramon.log(f"---|---|--->modul weight scale dtype: {module.weight_scale.dtype}")
             ramon.log(f"---|---|--->modul weight scale shape: {module.weight_scale.shape}")
-            ramon.log(f"---|---|--->modul dtype: {type(module.alpha)}")
+            ramon.log(f"---|---|--->modul alpha: {type(module.alpha)}")
 
             ramon.log("---|---|--->Right before nvfp4 gemm (non cute)")
             output = torch.ops.trtllm.nvfp4_gemm(act_fp4, module.weight, act_sf,
