8519d6b79b14a90288a15d01942136f860d5af12
Ramon Prieto <rprieto@etched.com>
2025-10-29 13:54:14 -0700

    update

--- tensorrt_llm/_torch/modules/linear.py
+++ tensorrt_llm/_torch/modules/linear.py
@@ -814,10 +814,12 @@ class NVFP4LinearMethod(LinearMethodBase):
                 module.scalar_alpha, module.dtype)
         else:
             ramon.log("---|---|--->either cutlass is not available or we'll not use cute nvfp4mm")
+            ramon.log(f"---|---|--->modul dtype: {module.dtype}")
             ramon.log(f"---|---|--->modul weight dtype: {module.weight.dtype}")
             ramon.log(f"---|---|--->modul weight shape: {module.weight.shape}")
             ramon.log(f"---|---|--->modul weight scale dtype: {module.weight_scale.dtype}")
             ramon.log(f"---|---|--->modul weight scale shape: {module.weight_scale.shape}")
+            ramon.log(f"---|---|--->modul dtype: {type(module.alpha)}")
 
             ramon.log("---|---|--->Right before nvfp4 gemm (non cute)")
             output = torch.ops.trtllm.nvfp4_gemm(act_fp4, module.weight, act_sf,
