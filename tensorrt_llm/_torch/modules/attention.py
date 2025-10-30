dac244b293a7b03afa5d70a155a5c20b832d96cb
Ramon Prieto <rprieto@etched.com>
2025-10-30 16:16:40 -0700

    update

--- tensorrt_llm/_torch/modules/attention.py
+++ tensorrt_llm/_torch/modules/attention.py
@@ -555,6 +555,9 @@ class Attention(nn.Module):
 
         ramon.log("Right before applying rope")
         q, k, v = self.apply_rope(q, k, v, position_ids)
+        assert q is not None
+        assert k is not None
+        assert v is not None
         ramon.log("Right before converting qkv")
         q, k, v = self.convert_qkv(q, k, v)
 
