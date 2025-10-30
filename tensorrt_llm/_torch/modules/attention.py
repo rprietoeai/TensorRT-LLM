5054b814f359e3c78090edaa99b62d1a9a668ab3
Ramon Prieto <rprieto@etched.com>
2025-10-30 16:06:45 -0700

    update

--- tensorrt_llm/_torch/modules/attention.py
+++ tensorrt_llm/_torch/modules/attention.py
@@ -571,7 +571,6 @@ class Attention(nn.Module):
         ramon.log(f"after rope: k shape: {k.shape}")
         ramon.log(f"after rope: v dtype: {v.dtype}")
         ramon.log(f"after rope: v shape: {v.shape}")
-        ramon.log("Right before converting qkv")
 
         if attention_sinks is not None:
             assert self.attn_backend == "TRTLLM", "Attention sinks are only supported for TRTLLM backend."
