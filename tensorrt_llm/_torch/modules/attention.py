0bb9d29d1ba2199fb7dcbff6042ccbaf269e1d99
Ramon Prieto <rprieto@etched.com>
2025-10-30 16:54:21 -0700

    update

--- tensorrt_llm/_torch/modules/attention.py
+++ tensorrt_llm/_torch/modules/attention.py
@@ -494,6 +494,8 @@ class Attention(nn.Module):
                 output = Fp4QuantizedTensor(output, output_sf)
 
         print(f"type of attention output: {type(output)}")
+        print(f"output: {output.dtype}")
+        print(f"output shape: {output.shape}")
         return output
 
     def forward(
