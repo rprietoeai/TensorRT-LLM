18a95c47ac65dfc3a2e2ac03e43cc4cbe95f92a7
Ramon Prieto <rprieto@etched.com>
2025-10-30 16:17:34 -0700

    update

--- tensorrt_llm/_torch/modules/attention.py
+++ tensorrt_llm/_torch/modules/attention.py
@@ -558,6 +558,12 @@ class Attention(nn.Module):
         assert q is not None
         assert k is not None
         assert v is not None
+        ramon.log(f"q: {q.dtype}")
+        ramon.log(f"q: {q.shape}")
+        ramon.log(f"k: {k.dtype}")
+        ramon.log(f"k: {k.shape}")
+        ramon.log(f"v: {v.dtype}")
+        ramon.log(f"v: {v.shape}")
         ramon.log("Right before converting qkv")
         q, k, v = self.convert_qkv(q, k, v)
 
