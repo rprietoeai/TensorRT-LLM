0b79efa1975c7660a908ea1fbdc31ee1c1296484
Ramon Prieto <rprieto@etched.com>
2025-10-31 14:06:32 -0700

    update

--- cpp/tensorrt_llm/thop/attentionOp.cpp
+++ cpp/tensorrt_llm/thop/attentionOp.cpp
@@ -657,7 +657,7 @@ void attention(torch::Tensor q, std::optional<torch::Tensor> k, std::optional<to
     op->mKVCacheQuantMode = tensorrt_llm::common::QuantMode(uint32_t(quant_mode));
     bool ramon_is_kv_nvfp4 = op->mKVCacheQuantMode == tensorrt_llm::common::QuantMode::nvfp4();
     RAMON_LOG("---|---|--->is kv cache quant mode is nvfp4: " << ramon_is_kv_nvfp4);
-    RAMON_LOG("---|---|--->op->mKVCacheQuantMode.mValue ");
+    RAMON_LOG("---|---|--->op->mKVCacheQuantMode.mValue: " << op->mKVCacheQuantMode.mValue);
     op->mUseKVCache = use_kv_cache;
     RAMON_LOG("---|---|--->use kv cache: " << op->mUseKVCache);
     op->mPagedKVCache = op->mPagedKVCache && use_kv_cache; // update mPagedKVCache based on use_kv_cache
