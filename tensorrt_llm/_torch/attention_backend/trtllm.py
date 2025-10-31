c584a530383238d84e81538bf087539d75bd2177
Ramon Prieto <rprieto@etched.com>
2025-10-31 11:04:13 -0700

    update

--- tensorrt_llm/_torch/attention_backend/trtllm.py
+++ tensorrt_llm/_torch/attention_backend/trtllm.py
@@ -1232,7 +1232,7 @@ class TrtllmAttention(AttentionBackend[TrtllmAttentionMetadata]):
         chunked_prefill_buffer_batch_size: int = 1,
         **kwargs,
     ) -> Union[torch.Tensor, Tuple[torch.Tensor, Optional[torch.Tensor]]]:
-        ramon.log("--->InsideFunction")
+        ramon.log("--->Inside unction")
         assert isinstance(
             metadata,
             TrtllmAttentionMetadata,
