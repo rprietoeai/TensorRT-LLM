3213dee097647934a0f13eb509fca2d4a62db2a6
Ramon Prieto <rprieto@etched.com>
2025-11-06 12:30:29 -0800

    update

--- cpp/tensorrt_llm/thop/attentionOp.cpp
+++ cpp/tensorrt_llm/thop/attentionOp.cpp
@@ -843,6 +843,7 @@ void attention(torch::Tensor q, std::optional<torch::Tensor> k, std::optional<to
     {
         auto seq_offset = 0;
         auto token_offset = 0;
+        RAMON_LOG("---|---|--->qkv_or_q: " << qkv_or_q);
         RAMON_LOG("---|---|--->right before runner->run, case 0");
         runner->run(*op,
             /*is_context=*/true, seq_offset,
